451af49841e7f173ebe0c9ae77b894419c60d653
haedanui <rhs9705kr@naver.com>
2023-06-11 00:50:49 +0900

    중복함수 제거중

--- src/main/java/spring_study/board_crud/api/CrawlingApiController.java
+++ src/main/java/spring_study/board_crud/api/CrawlingApiController.java
@@ -25,90 +25,20 @@ public class CrawlingApiController {
             
             // 원하는 데이터 추출 (예시: 제목 요소 추출)
             Elements titleElement = doc.select("h1");
-            String title = titleElement.text();
+            Elements pElement = doc.select("p");
+            String[] crawler = new String[5];
+            crawler[0] = titleElement.text();
+            crawler[1] = pElement.text();
+            //String title = titleElement.text();
+            //String p = titleElement.text();
             
             // API 응답으로 반환할 데이터 형식 구성 (예시: JSON 형식)
-            String jsonData = "{ \"title\": \"" + title + "\" }";
+            String jsonData = "{ \"title\": \"" + crawler[0] + "\" }";
             
             return jsonData;
         } catch (Exception e) {
             e.printStackTrace();
             return "Error occurred during data crawling.";
         }
-    }
-
-        @GetMapping("/api/pcrawling")
-    public String getpData() {
-        try {
-            // 크롤링할 사이트의 URL 설정
-            String url = "https://www.iana.org/domains/reserved";
-            
-            // jsoup을 사용하여 웹 페이지에 접근하여 데이터 크롤링
-            Document doc = Jsoup.connect(url).get();
-            
-            // 원하는 데이터 추출 (예시: 제목 요소 추출)
-            Elements titleElement = doc.select("p");
-            String title = titleElement.text();
-            
-            // API 응답으로 반환할 데이터 형식 구성 (예시: JSON 형식)
-            String jsonData = "{ \"title\": \"" + title + "\" }";
-            
-            return jsonData;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Error occurred during data crawling.";
-        }
-
-        
-    }
-
-            @GetMapping("/api/acrawling")
-    public String getaData() {
-        try {
-            // 크롤링할 사이트의 URL 설정
-            String url = "https://www.iana.org/domains/reserved";
-            
-            // jsoup을 사용하여 웹 페이지에 접근하여 데이터 크롤링
-            Document doc = Jsoup.connect(url).get();
-            
-            // 원하는 데이터 추출 (예시: 제목 요소 추출)
-            Elements titleElement = doc.select("a");
-            String title = titleElement.text();
-            
-            // API 응답으로 반환할 데이터 형식 구성 (예시: JSON 형식)
-            String jsonData = "{ \"title\": \"" + title + "\" }";
-            
-            return jsonData;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Error occurred during data crawling.";
-        }
-
-        
-    }
-
-                @GetMapping("/api/imgcrawling")
-    public String getimgData() {
-        try {
-            // 크롤링할 사이트의 URL 설정
-            String url = "https://www.iana.org/domains/reserved";
-            
-            // jsoup을 사용하여 웹 페이지에 접근하여 데이터 크롤링
-            Document doc = Jsoup.connect(url).get();
-            
-            // 원하는 데이터 추출 (예시: 제목 요소 추출)
-            Elements titleElement = doc.select("a");
-            String title = titleElement.text();
-            
-            // API 응답으로 반환할 데이터 형식 구성 (예시: JSON 형식)
-            String jsonData = "{ \"title\": \"" + title + "\" }";
-            
-            return jsonData;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Error occurred during data crawling.";
-        }
-
-        
     }
 }
