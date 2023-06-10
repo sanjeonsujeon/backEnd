1720f7bfaa6d19d4d94ae4bcf5bd667829bd7d1d
haedanui <rhs9705kr@naver.com>
2023-06-11 02:22:52 +0900

    동적웹을 쓰지 못하는 jsoup RIP

--- src/main/java/spring_study/board_crud/api/CrawlingApiController.java
+++ src/main/java/spring_study/board_crud/api/CrawlingApiController.java
@@ -18,13 +18,13 @@ public class CrawlingApiController {
     public String geth1Data() {
         try {
             // 크롤링할 사이트의 URL 설정
-            String url = "https://www.iana.org/domains/reserved";
+            String url = "https://www.google.com";
             
             // jsoup을 사용하여 웹 페이지에 접근하여 데이터 크롤링
             Document doc = Jsoup.connect(url).get();
             
             // 원하는 데이터 추출 (예시: 제목 요소 추출)
-            Elements titleElement = doc.select("h1");
+            Elements titleElement = doc.select("a");
             Elements pElement = doc.select("p");
             String[] crawler = new String[5];
             crawler[0] = titleElement.text();
@@ -33,7 +33,7 @@ public class CrawlingApiController {
             //String p = titleElement.text();
             
             // API 응답으로 반환할 데이터 형식 구성 (예시: JSON 형식)
-            String jsonData = "{ \"title\": \"" + crawler[0] + "\" }";
+            String jsonData = "{ \"title\": \"" + crawler[0] + "\", \"p\": \""+ crawler[1] + "\" }";
             
             return jsonData;
         } catch (Exception e) {
