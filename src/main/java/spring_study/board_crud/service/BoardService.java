539a1a1d4f07a07d44011270dec7950bd0015a10
haedanui <rhs9705kr@naver.com>
2023-06-12 02:40:55 +0900

    임시 커밋이에용

--- src/main/java/spring_study/board_crud/service/BoardService.java
+++ src/main/java/spring_study/board_crud/service/BoardService.java
@@ -1,9 +1,14 @@
 package spring_study.board_crud.service;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import spring_study.board_crud.domain.Board;
+import spring_study.board_crud.dto.BoardDto;
 import spring_study.board_crud.repository.BoardRepository;
 
 import java.util.List;
@@ -34,5 +39,12 @@ public class BoardService {
     @Transactional
     public void delete(Board board) {
         boardRepository.delete(board);
+    }
+
+    public Page<BoardDto> paging(Pageable pageable) {
+        int page = pageable.getPageNumber() -1;
+        int pageLimit = 10; // 한 페이지에 보여줄 글 갯수
+        Page<BoardEntity> boardEntities = 
+        boardRepository.findAll(PageRequest.of(page, pageLimit, Sort.by(Sort.Direction.DESC, "id")));
     }
 }
