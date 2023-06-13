1fbc55a1057ef209f31e1faba7e1c1ea278e231d
AoiTuNa <bestspark123@naver.com>
2023-06-14 03:49:46 +0900

    일단 로그인 토큰 발급은 구현 인증 제작중

--- src/main/java/spring_study/board_crud/api/MemberController.java
+++ src/main/java/spring_study/board_crud/api/MemberController.java
@@ -2,10 +2,12 @@ package spring_study.board_crud.api;
 
 import java.nio.channels.MembershipKey;
 
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -30,28 +32,30 @@ public class MemberController {
     public ResponseEntity login(@RequestBody MemberDTO memberDTO) {
         if (memberService.PWchecking(memberDTO)) {
             TokenProvider tokenProvider = new TokenProvider();
-            String authorization = tokenProvider.createToken(700000);
+            String authorization = tokenProvider.createAccessToken(700000,memberDTO.getUserid());
             String refreshtoken = tokenProvider.createToken(1800000);
             System.out.println(authorization);
             System.out.println(refreshtoken);
+            memberService.saveRefreshToken(memberDTO.getUserid(), refreshtoken);
             return ResponseEntity.status(200)
-                    .header("Authorization", tokenProvider.createToken(700000))
+                    .header("Authorization", tokenProvider.createAccessToken(700000,memberDTO.getUserid()))
                     .header("Refreshtoken", tokenProvider.createToken(1800000))
                     .build();
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ID_PW 불일치");
         }
     }
-
+    /* 
     @PostMapping("api/oauth/token")
     public ResponseEntity refresh(@RequestHeader("authorization") String authorization,
-            @RequestHeader("refreshtoken") String refreshtoken) {
+            @RequestHeader("refreshtoken") String refreshtoken, Authentication authentication) {
         TokenProvider tokenProvider = new TokenProvider();
+        String str = "ppap";
         System.out.println(authorization);
         System.out.println(refreshtoken);
-
-        if (tokenProvider.validateToken(authorization) && tokenProvider.validateToken(refreshtoken)) {
-            String Authorization = tokenProvider.createToken(700000);
+        System.out.println(authentication.getUsername());
+        if (tokenProvider.validateToken(authorization)) {
+            String Authorization = tokenProvider.createAccessToken(700000,str);
             String RefreshToken = tokenProvider.createToken(1800000);
             return ResponseEntity.status(200)
                     .header("Authorization", Authorization)
@@ -60,7 +64,14 @@ public class MemberController {
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("토큰 갱신 불가");
         }
-    }
+    }*/
+   /* @GetMapping("/my")
+    public MemberDTO findUser(Authentication authentication) throws Exception {
+        if (authentication == null) {
+            throw new Exception("회원 정보를 찾을 수 없습니다.");
+        }
+        return memberService.findUserid(authentication.getUsername());
+    }*/
 
     // 회원가입 API
     @PostMapping("api/signup-pp")
