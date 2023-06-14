c9c3e5d6e8ad27857d49173a2f1c7429ec29c6f4
AoiTuNa <bestspark123@naver.com>
2023-06-15 05:05:30 +0900

    게시판 페이지 번호매기기 완성

--- src/main/java/spring_study/board_crud/api/BoardApiController.java
+++ src/main/java/spring_study/board_crud/api/BoardApiController.java
@@ -1,43 +1,58 @@
 package spring_study.board_crud.api;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import spring_study.board_crud.domain.Board;
+
+import lombok.RequiredArgsConstructor;
 import spring_study.board_crud.dto.BoardDeleteDto;
-import spring_study.board_crud.dto.BoardDto;
 import spring_study.board_crud.service.BoardService;
+import spring_study.board_crud.domain.Board;
+import spring_study.board_crud.dto.BoardDto;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000/")
 public class BoardApiController {
-
     private final BoardService boardService; // Autowired로 스프링 빈에 등록
 
-    //게시판 띄우기
-    @GetMapping("/api/board-list")
-    public WrapperClass board_list() {
-        List<Board> boardList = boardService.findBoards();
+    @GetMapping("/api/total-ids")
+    public void getTotalIds(HttpServletResponse response) {
+        String totalcount = Long.toString(boardService.getTotalIds());
+        response.setHeader("totalcount", totalcount);
+    }
+
+    // 게시판 띄우기
+    @GetMapping("/api/board-list") // 게시판 띄우기
+    public WrapperClass board_list(@RequestParam("pageNumber") int pageNumber,
+            @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        pageable = PageRequest.of(pageNumber, pageable.getPageSize(), pageable.getSort());
+        Page<Board> boardList = boardService.findBoards(pageable);
         List<BoardDto> boardDtoList = boardList.stream().map(b -> new BoardDto(b)).collect(Collectors.toList());
         return new WrapperClass(boardDtoList);
     }
 
-    @GetMapping("/api/board-detail/{boardId}") //게시글 상세보기
+    @GetMapping("/api/board-detail/{boardId}") // 게시글 상세보기
     public WrapperClass board_detail(@PathVariable("boardId") Long boardId) {
         Board board = boardService.findOne(boardId);
         BoardDto boardDto = new BoardDto(board);
         return new WrapperClass(boardDto);
     }
 
-    @PostMapping("/api/create-board") //게시글 작성
+    @PostMapping("/api/create-board") // 게시글 작성
     public ResponseEntity create_board(@RequestBody BoardDto boardDto) {
         System.out.println("create_board/boardDto = " + boardDto);
         HttpHeaders headers = new HttpHeaders();
@@ -47,8 +62,7 @@ public class BoardApiController {
             Board board = new Board(
                     boardDto.getId(),
                     boardDto.getTitle(),
-                    boardDto.getContent()
-            );
+                    boardDto.getContent());
             boardService.create(board);
         } catch (Exception exception) {
             status = HttpStatus.BAD_REQUEST; // 400 에러
@@ -57,7 +71,7 @@ public class BoardApiController {
         return new ResponseEntity(body, headers, status);
     }
 
-    @PutMapping("/api/update-board") //게시글 수정
+    @PutMapping("/api/update-board") // 게시글 수정
     public ResponseEntity update_board(@RequestBody BoardDto boardDto) {
         System.out.println("update_board/boardDto = " + boardDto);
         HttpHeaders headers = new HttpHeaders();
@@ -72,7 +86,7 @@ public class BoardApiController {
         return new ResponseEntity(body, headers, status);
     }
 
-    @DeleteMapping("/api/delete-board") //게시글 삭제
+    @DeleteMapping("/api/delete-board") // 게시글 삭제
     public ResponseEntity delete_board(@RequestBody BoardDeleteDto boardDeleteDto) {
         System.out.println("delete_board/boardDeleteDto = " + boardDeleteDto);
         HttpHeaders headers = new HttpHeaders();
